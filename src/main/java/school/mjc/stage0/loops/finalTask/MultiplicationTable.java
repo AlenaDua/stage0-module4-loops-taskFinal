fb0cb8d1c36caae330aff92d55930ea22abec255
Alena Dubadzelava <dubodelova.minsk@gmail.com>
2023-02-13 12:40:10 +0100

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public void printTable(int numberTableToPrint) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+             {
+               sum = i * numberTableToPrint;
+
+            System.out.print(i + " x " + numberTableToPrint + " = " + sum);
+                 System.out.println();
+
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        int sum = 1;
+        for (int i = 1; i <= power; i++) {
+            sum = sum * numberToPrint;
+        }
+        System.out.print(sum);
+        System.out.println();
 
     }
 
